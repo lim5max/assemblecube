159c21a6d9d08543ebb69df1475f89da76ad28a3
lim5max <44651051+lim5max@users.noreply.github.com>
2019-01-20 16:23:08 +0300

    vfsg

--- Include/mysit/mysit/static/js.js
+++ Include/mysit/mysit/static/js.js
@@ -14,6 +14,9 @@ function fillEdge(color) {
 }
 
 colors= {blue: 1, orange: 2, green: 3, red: 4, white: 5, yellow: 6}
+
+colorToStyle = {1 : '#2C6E96' , 2: '#ff7f1d', 3:  '#7ce544',4:  '#ff0808' , 5: '#fffbfb' ,6:  '#fff339'}
+
 console.log(fillEdge(colors.green))
 cub = {
     first_blue: fillEdge(colors.blue),
@@ -24,8 +27,14 @@ cub = {
     sixth_yellow:  fillEdge(colors.yellow)
 
 }
-
+first   =       1
+second  =       2
+tird    =       3
+fourth  =       4 
+fifth   =       5
+sixth   =       6
 function draw(){
+
     const canvas = document.querySelector('canvas');
     const main_div = document.querySelector('.objects2d');
     // main_div.style.width = 500 + 'px';
@@ -36,29 +45,42 @@ function draw(){
     canvas.height = 700;
     console.log(main_div)
     var edgeNames = [
-        'first_blue',
-         'second_orange',
-          'third_green',
-           'fourth_red',
-            'fifth_white', 'sixth_yellow'];
-    edgeNames.forEach(function(name, index){
+        ['first_blue', [5, 105], ['first']],
+         ['second_orange', [105, 105],['second']],
+          ['third_green', [205, 105],['tird']],
+          ['fourth_red', [305, 105],['fourth']],
+            ['fifth_white', [205, 5],['fifth']], 
+            ['sixth_yellow' , [205, 205],['sixth']]
+        ];
+    edgeNames.forEach(function(edge ){
+        name = edge[0];
+        var sideX = edge[1][0];
+        var sideY = edge[1][1];
+        namepath  = edge[1][2] 
         console.log(name);
-        console.log(index);
+         
+        
       
         if (canvas.getContext) {
           var ctx = canvas.getContext('2d');
 
           
-          var sideX = 5 + 100 * index, sideY = 5;
-          var rectangle = new Path2D();
+           
           for (var x = 0; x < 3; x++){
+
             
             for (var y = 0; y<3; y++){
-                rectangle.rect(sideX + x * 30, sideY + y * 30, 20, 20);
+                
+                console.log("mbbn")
+                let color = cub[name][x][y];
+                console.log("mccn" + color)
+                let fillStylec = colorToStyle[color];  
+                ctx.fillStyle = fillStylec;
+                ctx.fillRect(sideX + x * 30, sideY + y * 30, 20, 20);
            
             }
           }
-          ctx.stroke(rectangle);
+          
         }
     })
 }
@@ -68,11 +90,43 @@ function pressed(e)
 {
  
     key = e.keyCode || e.which;
-    console.log(key)
+    if (key == 1066)  {  //shift + ]
+        switch(key){
+            case 49:  
+            case 50: 
+            case 51: 
+            case 52: 
+            case 53: 
+            case 54: 
+        }
+        if (key == 70){// shift+front
+
+        }
+        else if (key == 98){ //shift+back
     
- 
- 
- 
+        }
+       
+        else if  (key == 76) {//shift + LEFT 
+    
+        }
+        else if (key == 82){// shift +right
+            
+        }
+        else if (key == 85){// shift + up
+            
+        }
+        else if (key == 68){ //shift + down
+            
+        }
+        else if (key == 102){ //front
+            
+        }  
+        else break;
+    }
+   
+    
+    
+   
 }
 window.onload = function()
 
